c9d771f3aeab05107bc00f0ad83ab72cb1740047
dgtalbug <dgtalbug@users.noreply.github.com>
2026-03-06 18:38:50 +0000

    deploy: b70f903502ae630f4315f4c970e87e41b0bfd6bf

--- specs-site/assets/chunks/classDiagram-v2-MQ7JQ4JX.Bk8Qc7BR.js
+++
@@ -1,1 +0,0 @@
-import{s,c as e,a as r,b as t}from"./chunk-T2TOU4HS.BBi4WSAo.js";import{_ as l}from"../app.CbJviCA-.js";import"./chunk-5HRBRIJM.DqW5T0On.js";import"./framework.CTqRDTHW.js";import"./theme.Cl3ZpiuA.js";var p={parser:t,db:r,renderer:e,styles:s,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute,r.clear()},"init")};export{p as diagram};
